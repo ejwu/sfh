09b6dc831962a33533177dfb379d57be051080a4
Eric Wu <eric.j.wu@ggmail.com>
2014-11-28 17:45:14 -0800

    Skeletal game state, change runner for resistance

--- src/sfh/SfhRunner.java
+++ src/sfh/SfhRunner.java
@@ -10,63 +10,19 @@ import sfh.games.hulhe.HulheGameState;
 import sfh.games.hulhe.IpStrategy;
 import sfh.games.hulhe.OopStrategy;
 
+import sfh.games.resistance.*;
+
 import com.google.common.collect.Lists;
 
 public class SfhRunner {
 
     public static void main(String[] args) {
-        StateTableEvaluator.initialize();
 
-        Map<Long, Double> oopFrequencies = OopStrategy.createEqualFrequencies(
-            "JcJd", "JcJh", "JdJh",
-            "QcQd", "QcQh", "QcQs", "QdQh", "QdQs", "QhQs",
-            "KcKd", "KcKh", "KcKs", "KdKh", "KdKs", "KhKs",
-            "AcAd", "AcAh", "AcAs", "AdAh", "AdAs", "AhAs"
+      ResistanceGoodStrategy good = new ResistanceGoodStrategy();
+      ResistanceEvilStrategy evil = new ResistanceEvilStrategy();
+      ResistanceGameState gs = new ResistanceGameState();
 
-            /*
-				"7c7d", "7c7h", "7c7s", "7d7h", "7d7s", "7h7s",
-				//            "8d8h", "8d8s", "8h8s",
-				"9c9d", "9c9h", "9c9s", "9d9h", "9d9s", "9h9s",
-				"TcTd", "TcTh", "TcTs", "TdTh", "TdTs", "ThTs",
-				"JcJd", "JcJh", "JdJh",
-				"QcQd", "QcQh", "QcQs", "QdQh", "QdQs", "QhQs",
-				"KcKd", "KcKh", "KcKs", "KdKh", "KdKs", "KhKs",
-				"AcAd", "AcAh", "AcAs", "AdAh", "AdAs", "AhAs"
-             */
-            );
-
-        Map<Long, Double> ipFrequencies = IpStrategy.createEqualFrequencies(
-            "JcJd", "JcJh", "JdJh",
-            "QcQd", "QcQh", "QcQs", "QdQh", "QdQs", "QhQs",
-            "KcKd", "KcKh", "KcKs", "KdKh", "KdKs", "KhKs",
-            "AcAd", "AcAh", "AcAs", "AdAh", "AdAs", "AhAs"
-
-            /*
-				"8h6h", "8s6s",
-				"8d7d", "8h7h", "8s7s",
-				"8d9d", "8h9h", "8s9s",
-				"8dTd", "8hTh", "8sTs",
-				"Ad8d", "Ah8h", "As8s",
-				"Ac6c", "Ah6h", "As6s",
-				"7c6c", "7h6h", "7s6s",
-				"9c7c", "9d7d", "9h7h", "9s7s",
-				"7c5c", "7d5d", "7h5h", "7s5s",
-				"7c7d", "7c7h", "7c7s", "7d7h", "7d7s", "7h7s",
-				// add some real hands
-				"8d8h", "8d8s", "8h8s",
-				"6c6h", "6c6s", "6h6s"
-				//            "5c4c", "5d4d", "5h4h", "5s4s"
-             */
-            );
-
-        OopStrategy oop = OopStrategy.create(oopFrequencies);
-        IpStrategy ip = IpStrategy.create(ipFrequencies);
-
-        HulheGameState gs =
-            new HulheGameState(10.0, Deck.parseCardMask("8c6d2hJs2s"), oopFrequencies, ipFrequencies);
-        System.out.println("Game state:\n" + gs);
-
-        play(10, gs, oop, ip);
+      play(10, gs, good, evil);
     }
 
     public static <GS extends GameState<? super H,? super V>, H extends Strategy<GS,H,V>, V extends Strategy<GS,V,H>> void play(int iterations, GS gs, H strategy1, V strategy2) {
--- src/sfh/games/resistance/ResistanceGameState.java
+++ src/sfh/games/resistance/ResistanceGameState.java
@@ -1,15 +1,64 @@
 package sfh.games.resistance;
 
+import com.google.common.collect.*;
+
 import sfh.GameState;
+
+import java.util.*;
 
 public class ResistanceGameState implements GameState<ResistanceGoodStrategy, ResistanceEvilStrategy> {
 
-    public ResistanceGameState() {
+  private static final int TURNS_TO_WIN = 3;
+  
+  public static class Turn {
+    private Set<String> teamPlayers = Sets.newHashSet();
+    private Boolean missionPassed = null;
+
+    public Turn(boolean missionPassed, String... teamPlayers) {
+      this.missionPassed = missionPassed;
+      if (teamPlayers.length < 2) {
+        throw new IllegalArgumentException("team must be at least 2 players");
+      }
+      for (String teamPlayer : teamPlayers) {
+        if (this.teamPlayers.contains(teamPlayer)) {
+          throw new IllegalArgumentException("team can't repeat players");
+        }
+        this.teamPlayers.add(teamPlayer);
+      }
     }
+
+    public boolean isPassed() {
+      return missionPassed;
+    }
+
+    public Set<String> getTeam() {
+      return teamPlayers;
+    }
+  }
+
+  private List<Turn> previousTurns = Lists.newArrayList();
+  private int numPassed = 0;
+  private int numFailed = 0;
+
+  public ResistanceGameState() {
+  }
     
-    @Override
-    public double getValue(ResistanceGoodStrategy p1, ResistanceEvilStrategy p2) {
-            return 0.0;
+  public void addTurn(Turn turn) {
+    previousTurns.add(turn);
+    if (turn.isPassed()) {
+      numPassed++;
+    } else {
+      numFailed++;
     }
+  }
+
+  @Override
+  public double getValue(ResistanceGoodStrategy p1, ResistanceEvilStrategy p2) {
+    int currentTurn = numPassed + numFailed;
+
+    
+
+    return 0.0;
+  }
 
 }
